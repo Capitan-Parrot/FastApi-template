07409e6dde0160686a1aa4fd43e26590691f1c4d
Андрей <kondrandr2004@yandex.ru>
2023-10-13 21:40:53 +0300

    Fix redis

--- app/services/users.py
+++ app/services/users.py
@@ -15,22 +15,22 @@ class UserService:
         self.cache = cache
 
     async def delete(self, user_id: int) -> None:
-        self.cache.delete(user_id)
+        # self.cache.delete(user_id)
         self.db.delete(user_id)
 
     async def update(self, user_id: int, user_data: UserBase) -> User:
-        user = await self.cache.get(user_id)
+        # user = await self.cache.get(user_id)
+        # if not user:
+        user = self.db.get_user_by_id(user_id)
         if not user:
-            user = self.db.get_user_by_id(user_id)
-            if not user:
-                raise HTTPException(status_code=400, detail="Пользователь не существует")
+            raise HTTPException(status_code=400, detail="Пользователь не существует")
 
         for key, value in user_data:
             if key == "password":
                 setattr(user, "hashed_password", get_password_hash(value))
             else:
                 setattr(user, key, value)
-        self.cache.set(user_id, user)
+        # self.cache.set(user_id, user)
         self.db.update(user)
 
         return user
@@ -41,7 +41,7 @@ class UserService:
             raise HTTPException(status_code=400, detail="Пользователь с такой почтой уже существует")
 
         curr_user = self.db.create(user)
-        self.cache.set(curr_user.id, curr_user)
+        # self.cache.set(curr_user.id, curr_user)
 
         return curr_user
 
@@ -50,14 +50,14 @@ class UserService:
         return users
 
     async def get_user_by_id(self, user_id: int) -> User:
-        user = await self.cache.get(user_id)
-        if not user:
-            user = self.db.get_user_by_id(user_id)
+        # user = await self.cache.get(user_id)
+        # if not user:
+        user = self.db.get_user_by_id(user_id)
         return user
 
     async def get_user_by_email(self, email: str) -> User:
         user = self.db.get_user_by_email(email)
-        self.cache.set(user.id, user)
+        # self.cache.set(user.id, user)
         return user
 
 
